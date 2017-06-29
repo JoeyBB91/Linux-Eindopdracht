949fa569363a889360883593a434130df5a97408
Joey <j.r.boum.bletterman@st.hanze.nl>
2017-06-29 16:06:15 +0200

    Delete munin_configure.sh

--- shell/munin_configure.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# Aanpassingen maken aan de munin-minion.
-exec "munin-node-configure --shell --families=contrib,auto | sh -x"
- # echo "cidr_allow 10.4.0.49/32" >> /etc/munin/munin-node.conf && \
-exec "/etc/init.d/munin-node start"
-exit
