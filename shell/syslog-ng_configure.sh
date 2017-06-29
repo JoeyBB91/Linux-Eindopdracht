88f9005ba55ef7c03d586989acc9141c391bd679
Joey <j.r.boum.bletterman@st.hanze.nl>
2017-06-29 16:39:10 +0200

    Update syslog-ng_configure.sh

--- shell/syslog-ng_configure.sh
+++ shell/syslog-ng_configure.sh
@@ -3,3 +3,4 @@ wget -qO /etc/syslog-ng/syslog-ng.conf "https://wiki.debian.org/DebianEdu/HowTo/
 sed -i 's/10.0.2.2/10.4.0.49/' /etc/syslog-ng/syslog-ng.conf
 wget -qO- "https://raw.githubusercontent.com/JoeyBB91/Linux-Eindopdracht/master/shell/cronjob_to_rm_logs.sh" | sh
 /etc/init.d/syslog-ng start
+exit
