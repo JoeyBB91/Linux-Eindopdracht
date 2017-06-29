5473deca6ca9077a6bcfcb9438290e371de4197c
Joey <j.r.boum.bletterman@st.hanze.nl>
2017-06-29 16:00:01 +0200

    Update munin_configure.sh

--- shell/munin_configure.sh
+++ shell/munin_configure.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Aanpassingen maken aan de munin-minion.
-  munin-node-configure --shell --families=contrib,auto | sh -x && \
+Exec=munin-node-configure --shell --families=contrib,auto | sh -x
  # echo "cidr_allow 10.4.0.49/32" >> /etc/munin/munin-node.conf && \
-  /etc/init.d/munin-node start
+Exec=/etc/init.d/munin-node start
 
