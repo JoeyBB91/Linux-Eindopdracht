da0742f74d380bb0232a36a9a682a98fbad8f08f
Joey <j.r.boum.bletterman@st.hanze.nl>
2017-06-29 16:36:15 +0200

    Update munin_configure.sh

--- shell/munin_configure.sh
+++ shell/munin_configure.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#munin-node-configure --shell --families=contrib,auto | sh -x
+munin-node-configure --shell --families=contrib,auto | sh -x
 echo "cidr_allow 10.4.0.49/32" >> /etc/munin/munin-node.conf
 /etc/init.d/munin-node start
 exit
