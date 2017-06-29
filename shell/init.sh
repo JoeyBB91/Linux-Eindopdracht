6096cc6e1998850de23529259fef3d51afd36fb8
Joey <j.r.boum.bletterman@st.hanze.nl>
2017-06-29 16:52:33 +0200

    Update and rename test.sh to init.sh

---
+++ shell/init.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+wget -qO- "https://raw.githubusercontent.com/JoeyBB91/Linux-Eindopdracht/master/shell/munin_configure.sh" | sh
+wget -qO- "https://raw.githubusercontent.com/JoeyBB91/Linux-Eindopdracht/master/shell/syslog-ng_configure.sh" | sh
+exit
